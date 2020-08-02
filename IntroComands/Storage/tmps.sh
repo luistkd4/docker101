130e2a2c7af197c5bf2fc5859be71aaac21b0746
Luis Miguel <miguel@umbler.com>
2020-08-02 20:45:51 -0300

    dd command

--- IntroComands/Storage/tmps.sh
+++ IntroComands/Storage/tmps.sh
@@ -1,2 +1,5 @@
 #Cria um volume para armazenar aquivos temporários, evitando armazenar dados na camada de escrita
  docker run -d --name tmptest --mount type=tmpfs,destination=/cache,tmpfs-size=1000000 busybox sleep 3600
+
+ #Comando dd para criar o arquivo de 1mb
+ dd if=/dev/zero of=output.file bs=1024 count=1024
